3df7662757b4b1ec0e8e9fa24c081db23b270a8d
Wesley Faveron <92687678+Faveron@users.noreply.github.com>
2022-04-11 17:12:01 -0300

    Finalizado aula 3
    Esse é o primeiro update com os códigos até a aula 3 do curso

---
+++ Codelab/src/test/java/br/com/ifsp/codelab/Codelab/CodelabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifsp.codelab.Codelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodelabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
