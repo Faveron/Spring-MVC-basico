d7c3ae09bd99b32fd0b849dd165d17c7470b6eb3
Wesley Faveron <92687678+Faveron@users.noreply.github.com>
2022-04-13 14:15:15 -0300

    Códigos do final da aula 4

--- Codelab/src/main/java/br/com/ifsp/codelab/Codelab/controllers/HelloController.java
+++ Codelab/src/main/java/br/com/ifsp/codelab/Codelab/controllers/HelloController.java
@@ -1,15 +1,33 @@
 package br.com.ifsp.codelab.Codelab.controllers;
 
-
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
 
 @Controller
-public class HelloController {
+public class HelloController 
+{
+	@GetMapping("/hello")
+    public ModelAndView hello() 
+	{
+        ModelAndView mav = new ModelAndView("hello");  
+        mav.addObject("nome", "Almeida");
+        return mav;
+    }	
 
-    @GetMapping("/hello")
-    public String hello()
+    @GetMapping("/hello-model")
+    public String hello(Model model)
     {
+    	model.addAttribute("nome", "Faveron");
     	return "hello";
+    }
+    
+    @GetMapping("/hello-servlet")
+    public String hello(HttpServletRequest request) 
+    {
+        request.setAttribute("nome", "Wesley");
+        return "hello";
     }
 }
